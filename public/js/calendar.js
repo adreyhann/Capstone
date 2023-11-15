9f27478e019ed9ddfbbd9850728d65b422e7663b
adreyhann <meliboadrian@gmail.com>
2023-11-16 00:38:44 +0800

    qw

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,20 +1,150 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var calendarEl = document.getElementById('calendar');
-
+  
 	var calendar = new FullCalendar.Calendar(calendarEl, {
-		selectable: true,
-		headerToolbar: {
-			left: 'prev,next today',
-			center: 'title',
-			right: 'dayGridMonth,timeGridWeek,timeGridDay',
-		},
-		dateClick: function (info) {
-			alert('clicked ' + info.dateStr);
-		},
-		select: function (info) {
-			alert('selected ' + info.startStr + ' to ' + info.endStr);
-		},
+	  selectable: true,
+	  droppable: true,
+	  headerToolbar: {
+		left: 'prev,next today',
+		center: 'title',
+		right: 'dayGridMonth,timeGridWeek,timeGridDay',
+	  },
+	  drop: function (info) {
+		var eventData = {
+		  title: prompt('Enter event title:'),
+		  start: info.dropDate,
+		  allDay: true,
+		};
+  
+		if (eventData.title) {
+		  // Send the event to the server to save
+		  fetch('/events', {
+			method: 'POST',
+			headers: {
+			  'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(eventData),
+		  })
+			.then(response => response.json())
+			.then(data => {
+			  eventData.id = data.id;
+			  calendar.addEvent(eventData);
+			})
+			.catch(err => {
+			  console.error(err);
+			});
+		}
+	  },
+	  dateClick: function (info) {
+		var eventData = prompt('Enter event title:');
+		if (eventData) {
+		  var event = {
+			title: eventData,
+			start: info.date,
+			allDay: true,
+		  };
+  
+		  // Send the event to the server to save
+		  fetch('/events', {
+			method: 'POST',
+			headers: {
+			  'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(event),
+		  })
+			.then(response => response.json())
+			.then(data => {
+			  event.id = data.id;
+			  calendar.addEvent(event);
+			})
+			.catch(err => {
+			  console.error(err);
+			});
+		}
+	  },
+	  eventClick: function (info) {
+		var action = prompt('Edit or Delete event (e/d)?');
+		if (action === 'e') {
+		  var newEventData = prompt('Edit event title:', info.event.title);
+		  if (newEventData) {
+			var event = info.event;
+			event.title = newEventData;
+  
+			// Update the event on the server
+			fetch('/events/' + event.id, {
+			  method: 'PUT',
+			  headers: {
+				'Content-Type': 'application/json',
+			  },
+			  body: JSON.stringify(event),
+			})
+			  .then(response => response.json())
+			  .then(data => {
+				calendar.updateEvent(event);
+			  })
+			  .catch(err => {
+				console.error(err);
+			  });
+		  }
+		} else if (action === 'd') {
+		  var deleteConfirmation = confirm('Are you sure you want to delete this event?');
+		  if (deleteConfirmation) {
+			// Delete the event from the server
+			fetch('/events/' + info.event.id, {
+			  method: 'DELETE',
+			})
+			  .then(response => response.json())
+			  .then(data => {
+				if (data.deleted) {
+				  calendar.removeEvent(info.event);
+				}
+			  })
+			  .catch(err => {
+				console.error(err);
+			  });
+		  }
+		}
+	  },
+	  eventDrop: function (info) {
+		info.event.start = info.dropDate;
+  
+		// Update the event on the server
+		fetch('/events/' + info.event.id, {
+		  method: 'PUT',
+		  headers: {
+			'Content-Type': 'application/json',
+		  },
+		  body: JSON.stringify(info.event),
+		})
+		  .then(response => response.json())
+		  .then(data => {
+			calendar.updateEvent(info.event);
+		  })
+		  .catch(err => {
+			console.error(err);
+		  });
+	  },
+	  eventResize: function (info) {
+		info.event.end = info.end;
+  
+		// Update the event on the server
+		fetch('/events/' + info.event.id, {
+		  method: 'PUT',
+		  headers: {
+			'Content-Type': 'application/json',
+		  },
+		  body: JSON.stringify(info.event),
+		})
+		  .then(response => response.json())
+		  .then(data => {
+			calendar.updateEvent(info.event);
+		  })
+		  .catch(err => {
+			console.error(err);
+		  });
+	  },
 	});
-
+  
 	calendar.render();
-});
+  });
+  
--- routes/system.admin.route.js
+++ routes/system.admin.route.js
@@ -1,17 +1,18 @@
 const router = require('express').Router();
 const multer = require('multer');
-const path = require('path')
+const path = require('path');
 const fs = require('fs');
 const { PDFDocument } = require('pdf-lib');
 const User = require('../models/user.model');
-const {Records, Archives} = require('../models/records.model');
+const { Records, Archives } = require('../models/records.model');
+const Event = require('../models/events.model');
 
 router.get('/dashboard', async (req, res, next) => {
 	// console.log(req.user)
 	const person = req.user;
 	const users = await User.find();
 	const records = await Records.find();
-    const archives = await Archives.find();
+	const archives = await Archives.find();
 	res.render('system_admn/dashboard', { person, users, records, archives });
 });
 
@@ -29,8 +30,8 @@ router.get('/records', async (req, res, next) => {
 
 router.get('/archives', async (req, res, next) => {
 	const person = req.user;
-    const archivedRecord = await Archives.find();
-	res.render('system_admn/archives', { person, archivedRecord});
+	const archivedRecord = await Archives.find();
+	res.render('system_admn/archives', { person, archivedRecord });
 });
 
 router.get('/calendar', async (req, res, next) => {
@@ -67,39 +68,37 @@ router.get('/addRecords', async (req, res, next) => {
 	res.render('system_admn/addRecords', { person });
 });
 
-
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/uploads'); 
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    },
+	destination: function (req, file, cb) {
+		cb(null, 'public/uploads');
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	},
 });
 
 const upload = multer({
-    storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, 
-    fileFilter: function (req, file, cb) {
-        
-        if (file.mimetype === 'application/pdf') {
-            cb(null, true);
-        } else {
-            cb(new Error('Only PDF files are allowed!'), false);
-        }
-    },
+	storage: storage,
+	limits: { fileSize: 5 * 1024 * 1024 },
+	fileFilter: function (req, file, cb) {
+		if (file.mimetype === 'application/pdf') {
+			cb(null, true);
+		} else {
+			cb(new Error('Only PDF files are allowed!'), false);
+		}
+	},
 });
 router.get('/view-files/:id', async (req, res, next) => {
-    try {
-        const studentId = req.params.id;
-        const student = await Records.findById(studentId);
+	try {
+		const studentId = req.params.id;
+		const student = await Records.findById(studentId);
 
 		console.log('Student:', student);
 
 		if (!student) {
-            res.status(404).send('Record not found');
-            return;
-        }
+			res.status(404).send('Record not found');
+			return;
+		}
 
 		const base64PDF = await Promise.all(
 			student.pdfFilePath.map(async (filePath) => {
@@ -113,18 +112,25 @@ router.get('/view-files/:id', async (req, res, next) => {
 				}
 			})
 		);
-		
+
 		// Assuming pdfFilePath is an array of file paths
 		const filename = base64PDF.map((_, index) => {
-			return student.pdfFilePath[index] ? path.basename(student.pdfFilePath[index]) : null;
+			return student.pdfFilePath[index]
+				? path.basename(student.pdfFilePath[index])
+				: null;
 		});
 
 		const person = req.user;
-        res.render('system_admn/view-files', { student, person, base64PDF, filename });
-    } catch (error) {
-		console.error('Error:', error); 
-        next(error);
-    }
+		res.render('system_admn/view-files', {
+			student,
+			person,
+			base64PDF,
+			filename,
+		});
+	} catch (error) {
+		console.error('Error:', error);
+		next(error);
+	}
 });
 
 router.post('/submit-form', upload.array('pdfFile'), async (req, res, next) => {
@@ -139,16 +145,19 @@ router.post('/submit-form', upload.array('pdfFile'), async (req, res, next) => {
 			return res.redirect('/systemAdmin/addRecords'); // Redirect to the upload page or handle it as needed
 		}
 
-        const filePaths = files.map(file => file.path);
+		const filePaths = files.map((file) => file.path);
 
 		if (!filePaths || filePaths.length === 0) {
-            req.flash('error', 'Uploading at least one file is required. Please select PDF files to upload.');
-            res.redirect('/systemAdmin/addRecords');
-            return;
-        }
+			req.flash(
+				'error',
+				'Uploading at least one file is required. Please select PDF files to upload.'
+			);
+			res.redirect('/systemAdmin/addRecords');
+			return;
+		}
 
 		console.log('Attempting to serve file:', filePaths);
-		
+
 		const newRecord = new Records({
 			lrn: parseInt(lrn),
 			studentName: name,
@@ -158,12 +167,11 @@ router.post('/submit-form', upload.array('pdfFile'), async (req, res, next) => {
 			pdfFilePath: filePaths,
 		});
 
-		
 		const savedRecord = await newRecord.save();
 
 		req.flash('success', `${savedRecord.studentName} is succesfully saved`);
 		res.redirect('/systemAdmin/addRecords');
-		res.redirect(`/systemAdmin/view-files/${savedRecord._id}`); 
+		res.redirect(`/systemAdmin/view-files/${savedRecord._id}`);
 	} catch (error) {
 		if (
 			error.code === 11000 &&
@@ -171,173 +179,176 @@ router.post('/submit-form', upload.array('pdfFile'), async (req, res, next) => {
 			error.keyPattern.lrn === 1
 		) {
 			req.flash('error', 'LRN already exists. Please enter a different LRN.');
-			res.redirect('/systemAdmin/addRecords'); 
+			res.redirect('/systemAdmin/addRecords');
 		} else {
 			next(error);
 		}
 	}
 });
 
-router.post('/addFile/:recordId', upload.single('pdfFile'), async (req, res, next) => {
-    try {
-        const recordId = req.params.recordId;
+router.post(
+	'/addFile/:recordId',
+	upload.single('pdfFile'),
+	async (req, res, next) => {
+		try {
+			const recordId = req.params.recordId;
 
-        // Find the record by ID
-        const record = await Records.findById(recordId);
+			// Find the record by ID
+			const record = await Records.findById(recordId);
 
-        if (!record) {
-            res.status(404).send('Record not found');
-            return;
-        }
+			if (!record) {
+				res.status(404).send('Record not found');
+				return;
+			}
 
-        // Check if a file was uploaded
-        if (req.file) {
-            // Check if the number of files is less than 2
-            if (record.pdfFilePath.length < 2) {
-                const newPdfPath = req.file.path;
+			// Check if a file was uploaded
+			if (req.file) {
+				// Check if the number of files is less than 2
+				if (record.pdfFilePath.length < 2) {
+					const newPdfPath = req.file.path;
 
-                // Update the existing record with the new file path
-                record.pdfFilePath.push(newPdfPath);
-                await record.save();
+					// Update the existing record with the new file path
+					record.pdfFilePath.push(newPdfPath);
+					await record.save();
 
-                // Redirect back to the view-files page
-                req.flash('success', 'Successfully uploaded');
-                res.redirect(`/systemAdmin/view-files/${recordId}`);
-            } else {
-                // Display a flash message for exceeding the maximum allowed files
-                req.flash('error', 'You can only upload up to 2 files.');
-                res.redirect(`/systemAdmin/view-files/${recordId}`);
-            }
-        } else {
-            req.flash('error', 'No file uploaded');
-            res.status(400).send('No file uploaded');
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        next(error);
-    }
-
-});
+					// Redirect back to the view-files page
+					req.flash('success', 'Successfully uploaded');
+					res.redirect(`/systemAdmin/view-files/${recordId}`);
+				} else {
+					// Display a flash message for exceeding the maximum allowed files
+					req.flash('error', 'You can only upload up to 2 files.');
+					res.redirect(`/systemAdmin/view-files/${recordId}`);
+				}
+			} else {
+				req.flash('error', 'No file uploaded');
+				res.status(400).send('No file uploaded');
+			}
+		} catch (error) {
+			console.error('Error:', error);
+			next(error);
+		}
+	}
+);
 
 router.post('/deleteFile/:recordId/:index', async (req, res, next) => {
-    try {
-        const recordId = req.params.recordId;
-        const index = req.params.index;
+	try {
+		const recordId = req.params.recordId;
+		const index = req.params.index;
 
-        // Find the record by ID
-        const record = await Records.findById(recordId);
+		// Find the record by ID
+		const record = await Records.findById(recordId);
 
-        if (!record) {
-            req.flash('error', 'Record not found');
-            res.redirect(`/systemAdmin/view-files/${recordId}`); // Redirect to an error route
-            return;
-        }
-
-        // Check if the index is valid
-        if (index >= 0 && index < record.pdfFilePath.length) {
-            // Remove the file path at the specified index
-            record.pdfFilePath.splice(index, 1);
-            await record.save();
-
-			// Set success flash message
-            req.flash('success', 'File successfully deleted');
-        } else {
-			// Set error flash message for an invalid file index
-            req.flash('error', 'Invalid file index');
+		if (!record) {
+			req.flash('error', 'Record not found');
+			res.redirect(`/systemAdmin/view-files/${recordId}`); // Redirect to an error route
+			return;
 		}
 
-        // Redirect back to the view-files page
-        res.redirect(`/systemAdmin/view-files/${recordId}`);
-    } catch (error) {
-        console.error('Error:', error);
-        next(error);
-    }
+		// Check if the index is valid
+		if (index >= 0 && index < record.pdfFilePath.length) {
+			// Remove the file path at the specified index
+			record.pdfFilePath.splice(index, 1);
+			await record.save();
+
+			// Set success flash message
+			req.flash('success', 'File successfully deleted');
+		} else {
+			// Set error flash message for an invalid file index
+			req.flash('error', 'Invalid file index');
+		}
+
+		// Redirect back to the view-files page
+		res.redirect(`/systemAdmin/view-files/${recordId}`);
+	} catch (error) {
+		console.error('Error:', error);
+		next(error);
+	}
 });
 
 // Add this route for handling record updates
 router.post('/edit-record/:recordId', async (req, res, next) => {
-    try {
-        const recordId = req.params.recordId;
+	try {
+		const recordId = req.params.recordId;
 
-        // Find the record by ID
-        const record = await Records.findById(recordId);
+		// Find the record by ID
+		const record = await Records.findById(recordId);
 
-        if (!record) {
-            res.status(404).send('Record not found');
-            return;
-        }
+		if (!record) {
+			res.status(404).send('Record not found');
+			return;
+		}
 
-        // Update the record with new values
+		// Update the record with new values
 		record.lrn = req.body.editLrn;
-        record.studentName = req.body.editName;
-        record.gender = req.body.editGender;
+		record.studentName = req.body.editName;
+		record.gender = req.body.editGender;
 		record.transferee = req.body.editTransferee;
-        record.gradeLevel = req.body.editGradeLevel;
+		record.gradeLevel = req.body.editGradeLevel;
 
-        // Save the updated record
-        await record.save();
+		// Save the updated record
+		await record.save();
 
-        // Redirect back to the records page
-        req.flash('success', 'Record updated successfully');
-        res.redirect('/systemAdmin/records');
-    } catch (error) {
-        console.error('Error:', error);
-        next(error);
-    }
+		// Redirect back to the records page
+		req.flash('success', 'Record updated successfully');
+		res.redirect('/systemAdmin/records');
+	} catch (error) {
+		console.error('Error:', error);
+		next(error);
+	}
 });
 
 // Add this route for moving records to the archive
 router.post('/move-to-archive/:recordId', async (req, res, next) => {
-    try {
-        const recordId = req.params.recordId;
+	try {
+		const recordId = req.params.recordId;
 
-        // Find the record by ID
-        const record = await Records.findById(recordId);
+		// Find the record by ID
+		const record = await Records.findById(recordId);
 
-        if (!record) {
-            res.status(404).send('Record not found');
-            return;
-        }
+		if (!record) {
+			res.status(404).send('Record not found');
+			return;
+		}
 
-        // Move the record to the ArchivedRecords collection
-        const archivedRecord = new Archives({
-            lrn: record.lrn,
-            studentName: record.studentName,
-            gender: record.gender,
-            dateAddedToArchive: new Date(),
-            pdfFilePath: record.pdfFilePath, // Check if you need to handle PDF files
-        });
+		// Move the record to the ArchivedRecords collection
+		const archivedRecord = new Archives({
+			lrn: record.lrn,
+			studentName: record.studentName,
+			gender: record.gender,
+			dateAddedToArchive: new Date(),
+			pdfFilePath: record.pdfFilePath, // Check if you need to handle PDF files
+		});
 
-        // Save the archived record
-        await archivedRecord.save();
+		// Save the archived record
+		await archivedRecord.save();
 
-        // Delete the record from the original collection
-        await Records.findByIdAndDelete(recordId);
+		// Delete the record from the original collection
+		await Records.findByIdAndDelete(recordId);
 
-        req.flash('success', 'Record moved to archive successfully');
-        res.redirect('/systemAdmin/records');
-    } catch (error) {
-        console.error('Error:', error);
-        // Handle errors appropriately, e.g., flash an error message
-        req.flash('error', 'Failed to move record to archive');
-        res.redirect('/systemAdmin/records');
-    }
+		req.flash('success', 'Record moved to archive successfully');
+		res.redirect('/systemAdmin/records');
+	} catch (error) {
+		console.error('Error:', error);
+		// Handle errors appropriately, e.g., flash an error message
+		req.flash('error', 'Failed to move record to archive');
+		res.redirect('/systemAdmin/records');
+	}
 });
 
 // Add this route for viewing files of an archived record
 router.get('/archived-files/:recordId', async (req, res, next) => {
-    try {
-        const recordId = req.params.recordId;
-        const student = await Archives.findById(recordId);
-        // Find the archived record by ID
-        const archivedRecord = await Archives.findById(recordId);
+	try {
+		const recordId = req.params.recordId;
+		const student = await Archives.findById(recordId);
+		// Find the archived record by ID
+		const archivedRecord = await Archives.findById(recordId);
 
-        if (!archivedRecord) {
-            res.status(404).send('Archived Record not found');
-            return;
-        }
+		if (!archivedRecord) {
+			res.status(404).send('Archived Record not found');
+			return;
+		}
 
-        const base64PDF = await Promise.all(
+		const base64PDF = await Promise.all(
 			student.pdfFilePath.map(async (filePath) => {
 				if (filePath) {
 					const pdfData = await fs.promises.readFile(filePath);
@@ -349,19 +360,71 @@ router.get('/archived-files/:recordId', async (req, res, next) => {
 				}
 			})
 		);
-		
+
 		// Assuming pdfFilePath is an array of file paths
 		const filename = base64PDF.map((_, index) => {
-			return student.pdfFilePath[index] ? path.basename(student.pdfFilePath[index]) : null;
+			return student.pdfFilePath[index]
+				? path.basename(student.pdfFilePath[index])
+				: null;
 		});
 
 		const person = req.user;
 
-        res.render('system_admn/archived-files', { archivedRecord, student, person, base64PDF, filename }); // Adjust the view name as needed
-    } catch (error) {
-        console.error('Error:', error);
-        next(error);
-    }
+		res.render('system_admn/archived-files', {
+			archivedRecord,
+			student,
+			person,
+			base64PDF,
+			filename,
+		}); // Adjust the view name as needed
+	} catch (error) {
+		console.error('Error:', error);
+		next(error);
+	}
+});
+
+// routes for calendar
+router.get('/events', (req, res) => {
+	Event.find({}, (err, events) => {
+		if (err) {
+			console.error(err);
+			res.status(500).send('Error fetching events');
+			return;
+		}
+
+		res.render('system_admn/calendar', { events });
+	});
+});
+
+router.post('/events', (req, res) => {
+	const event = new Event(req.body);
+	event.save((err, savedEvent) => {
+		if (err) {
+			console.error(err);
+			res.status(500).send('Error creating event');
+			return;
+		}
+
+		res.json({ id: savedEvent._id });
+	});
+});
+
+router.delete('/events/:id', (req, res) => {
+	const eventId = req.params.id;
+	Event.findByIdAndDelete(eventId, (err, deletedEvent) => {
+		if (err) {
+			console.error(err);
+			res.status(500).send('Error deleting event');
+			return;
+		}
+
+		if (!deletedEvent) {
+			res.status(404).send('Event not found');
+			return;
+		}
+
+		res.json({ deleted: true });
+	});
 });
 
 module.exports = router;

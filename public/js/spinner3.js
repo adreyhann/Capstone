d1ec35aa749cb0ed1afe25529686ff451761012d
adreyhann <meliboadrian@gmail.com>
2024-03-17 17:23:50 +0800

    added loading animation in reseting the password

--- public/js/spinner3.js
+++ public/js/spinner3.js
@@ -19,3 +19,47 @@ document.addEventListener('DOMContentLoaded', function () {
         submitButton.disabled = true;
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('forgotpass');
+    const submitButton = document.getElementById('submitButton');
+
+    form.addEventListener('submit', function () {
+        // Show the spinner when submitting the form
+        submitButton.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...';
+        submitButton.disabled = true;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('resetCode');
+    const submitButton = document.getElementById('submitButton');
+
+    form.addEventListener('submit', function () {
+        // Show the spinner when submitting the form
+        submitButton.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...';
+        submitButton.disabled = true;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('resetpass');
+    const submitButton = document.getElementById('submitButton');
+
+    form.addEventListener('submit', function () {
+        // Show the spinner when submitting the form
+        submitButton.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...';
+        submitButton.disabled = true;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('login');
+    const submitButton = document.getElementById('submitButton');
+
+    form.addEventListener('submit', function () {
+        // Show the spinner when submitting the form
+        submitButton.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Logging in...';
+        submitButton.disabled = true;
+    });
+});

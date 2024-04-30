78ff535c84cec4f101c990e9d74ccdc1f969bc74
adreyhann <meliboadrian@gmail.com>
2024-04-30 23:05:47 +0800

    a

--- public/js/select-all-archives.js
+++ public/js/select-all-archives.js
@@ -1,33 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Select All checkbox
-    const selectAllCheckbox = document.getElementById("selectAllArchive");
-    const archivedRecordCheckboxes = document.querySelectorAll('input[name="archivedRecords"]');
-    const unArchiveButton = document.getElementById("unArchiveButton");
+document.addEventListener('DOMContentLoaded', function () {
+	const selectAllCheckbox = document.getElementById('selectAllArchive');
+	const archivedRecordCheckboxes = document.querySelectorAll(
+		'input[name="archivedRecords"]'
+	);
+	const unArchiveButton = document.getElementById('unArchiveButton');
 
-    // Function to toggle all checkboxes based on the select all checkbox
-    function toggleCheckboxes() {
-        archivedRecordCheckboxes.forEach((checkbox) => {
-            checkbox.checked = selectAllCheckbox.checked;
-        });
-        updateUnArchiveButtonVisibility(selectAllCheckbox.checked);
-    }
+	function toggleCheckboxes() {
+		archivedRecordCheckboxes.forEach((checkbox) => {
+			checkbox.checked = selectAllCheckbox.checked;
+		});
+		updateUnArchiveButtonVisibility(selectAllCheckbox.checked);
+	}
 
-    // Function to update the state of the Unarchive button
-    function updateUnArchiveButtonVisibility(checked) {
-        unArchiveButton.style.display = checked ? "inline-block" : "none";
-    }
+	function updateUnArchiveButtonVisibility() {
+		const checkedCount = document.querySelectorAll(
+			'input[name="archivedRecords"]:checked'
+		).length;
+		unArchiveButton.style.display = checkedCount >= 2 ? 'inline-block' : 'none';
+	}
 
-    // Event listener for the select all checkbox
-    selectAllCheckbox.addEventListener("change", function () {
-        toggleCheckboxes();
-    });
+	selectAllCheckbox.addEventListener('change', function () {
+		toggleCheckboxes();
+	});
 
-    // Event listener for individual checkboxes
-    archivedRecordCheckboxes.forEach((checkbox) => {
-        checkbox.addEventListener("change", function () {
-            // If any checkbox is unchecked, uncheck the select all checkbox
-            selectAllCheckbox.checked = archivedRecordCheckboxes.length === document.querySelectorAll('input[name="archivedRecords"]:checked').length;
-            updateUnArchiveButtonVisibility(selectAllCheckbox.checked);
-        });
-    });
+	archivedRecordCheckboxes.forEach((checkbox) => {
+		checkbox.addEventListener('change', function () {
+			
+			selectAllCheckbox.checked =
+				archivedRecordCheckboxes.length ===
+				document.querySelectorAll('input[name="archivedRecords"]:checked')
+					.length;
+			updateUnArchiveButtonVisibility();
+		});
+	});
 });

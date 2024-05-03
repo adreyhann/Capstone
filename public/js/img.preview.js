d4aa72f74c1f110c83a80829988e841b370d1c1e
adreyhann <meliboadrian@gmail.com>
2024-05-03 08:31:40 +0800

    d

--- public/js/img.preview.js
+++ public/js/img.preview.js
@@ -1,14 +1,14 @@
 // Function to open the transparent card overlay and display the clicked image
 function openImagePreview(img) {
-    var card = document.getElementById("imagePreviewCard");
-    var previewedImage = document.getElementById("previewedImage");
-    var profilePicture = img.getAttribute('data-profile-picture');
-    previewedImage.src = profilePicture;
-    card.style.display = "block";
+	var card = document.getElementById('imagePreviewCard');
+	var previewedImage = document.getElementById('previewedImage');
+	var profilePicture = img.getAttribute('data-profile-picture');
+	previewedImage.src = profilePicture;
+	card.style.display = 'block';
 }
 
 // Function to close the transparent card overlay
 function closeImagePreview() {
-    var card = document.getElementById("imagePreviewCard");
-    card.style.display = "none";
+	var card = document.getElementById('imagePreviewCard');
+	card.style.display = 'none';
 }
